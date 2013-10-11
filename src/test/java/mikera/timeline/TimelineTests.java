0ed23fe07d991d06d5b6b2ed65f82e6badd23ace
mikera <mike.r.anderson.13@gmail.com>
2013-10-11 11:49:17 +0800

    add cljunit tests

---
+++ src/test/java/mikera/timeline/TimelineTests.java
@@ -0,0 +1,11 @@
+package mikera.timeline;
+
+import mikera.cljunit.ClojureTest;
+
+public class TimelineTests extends ClojureTest {
+	@Override
+	public String filter() {
+		return "mikera.timeline";
+	}
+
+}
