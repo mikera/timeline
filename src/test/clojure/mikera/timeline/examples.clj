c573accf0a6912da39f6fa4e82c571db548194d3
mikera <mike.r.anderson.13@gmail.com>
2013-10-15 19:51:31 +0800

    example edis

--- src/test/clojure/mikera/timeline/examples.clj
+++ src/test/clojure/mikera/timeline/examples.clj
@@ -6,8 +6,10 @@
 ;;
 ;; in which we use an atom to accumulate changes to a timeline
 
+;; put a timeline in an atom for logging
 (def event-log (atom (timeline)))
 
+;; add some events with a small delay between each
 (dotimes [i 10]
   (Thread/sleep (rand-int 100))
   (swap! event-log log (str "Event: i")))
